b349bc5d3905cdca2906ddcebda3515ff221a1ab
Vadym1288 <141283561+Vadym1288@users.noreply.github.com>
2024-02-15 22:40:44 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+function isEnoughCapacity(products, containerSize) {
+  let allProducts = 0;
+  for (const product in products) {
+    allProducts += products[product];
+  }
+  return allProducts <= containerSize ? true : false;
+}
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14));
+
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7));
